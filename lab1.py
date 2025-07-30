df852518167b9b3241614758e143e74411e69f7b
MR.KSHEERA SAGAR SHIVANSH <shivanshks2006@gmail.com>
2025-07-30 23:49:16 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,14 +1,13 @@
-def grades(mark):
-    mark = int(mark)
-    if 90 <= mark <= 100:
-        print("A")
-    elif 75 <= mark <= 89:
-        print("B")
-    elif 60 <= mark <= 74:
-        print("C")
-    elif 40 <= mark <= 59:
-        print("D")
-    elif 0 <= mark <= 39:
-        print("F")
-    else:
-        print("Invalid mark")
+mark = int(input())
+if 90 <= mark <= 100:
+    print("A")
+elif 75 <= mark <= 89:
+    print("B")
+elif 60 <= mark <= 74:
+    print("C")
+elif 40 <= mark <= 59:
+    print("D")
+elif 0 <= mark <= 39:
+    print("F")
+else:
+    print("Invalid mark")
