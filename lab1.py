b60a3e4255ee27842e85be0dfdc642a3d71c1c1a
MR.KSHEERA SAGAR SHIVANSH <shivanshks2006@gmail.com>
2025-07-30 22:26:16 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,3 +1,34 @@
-#Write your code here in python 
-n=int(input("enter a nuber"))
-print(n)
+# #Write your code here in python 
+# n=int(input("enter a nuber"))
+# print(n)
+#function to determine grades
+def grades(mark):
+    mark = int(mark)  #convert input to integer
+    if 90<=mark<=100:
+        #check for A grade
+        print("A")
+    elif 75<=mark<=89:
+        #check for B grade
+        print("B")
+    elif 60<=mark<=74:
+        #check for C grade
+        print("C")
+    elif 40<=mark<=59:
+        #check for D grade
+        print("D")
+    elif 0<=mark<=39:
+        #check for F grade
+        print("F")
+    else:
+        #if mark is invalid
+        print("Invalid mark")
+
+while True:  #infinite loop to take multiple inputs
+        mark = (input("Enter the marks or -1 to exit: "))   #taking input from user
+        if mark == '-1':   #exit condition
+            print("Exited successfully")
+            break
+        if mark.isdigit():  #check if input is a digit
+            grades(mark)   #function call
+        else:
+            print("Invalid marks")
